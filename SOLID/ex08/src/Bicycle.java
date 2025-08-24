6f9fda5ad08369b45019f0c722087beda22ab4d2
Shrimay <shrimaysomani18@gmail.com>
2025-08-25 01:52:02 +0530

    ft: ex08 completed

--- SOLID/ex08/src/Bicycle.java
+++ SOLID/ex08/src/Bicycle.java
@@ -1,5 +1,11 @@
-public class Bicycle implements Vehicle {
-    public void startEngine(){ throw new UnsupportedOperationException(); }
-    public void pedal(int effort){ System.out.println("Pedal effort " + effort); }
-    public void recharge(int kWh){ throw new UnsupportedOperationException(); }
+public class Bicycle{
+    private Pedalable pedalable;
+
+    public Bicycle(Pedalable pedalable) {
+        this.pedalable = pedalable;
+    }
+
+    public void pedal(int effort) {
+        pedalable.pedal(effort);
+    }
 }
---
+++ SOLID/ex08/src/Pedal.java
@@ -0,0 +1,3 @@
+public class Pedal implements Pedalable{
+    public void pedal(int effort){ System.out.println("Pedal effort " + effort); }    
+}
