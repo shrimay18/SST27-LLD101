107d109e176b78b5eb4e23328441b2901c9ea972
Shrimay <shrimaysomani18@gmail.com>
2025-08-25 02:20:36 +0530

    ft: ex10 completed

--- SOLID/ex10/src/Demo10.java
+++ SOLID/ex10/src/Demo10.java
@@ -1,5 +1,8 @@
 public class Demo10 {
     public static void main(String[] args) {
-        new ReportService().generate();
+        Logger logger = new ConsoleLogger();
+        ReportService service = new ReportService(logger);
+
+        service.generate();
     }
 }
--- SOLID/ex10/src/ReportService.java
+++ SOLID/ex10/src/ReportService.java
@@ -1,6 +1,12 @@
 public class ReportService {
-    void generate(){
-        ConsoleLogger logger = new ConsoleLogger();
+    private final Logger logger;
+
+    // Constructor injection ensures flexibility
+    public ReportService(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void generate() {
         logger.log("Generating daily report...");
         System.out.println("Report contents...");
     }
