d8334742e4f0576e8f23078d6176f3f7121702f0
Shrimay <shrimaysomani18@gmail.com>
2025-08-24 18:07:37 +0530

    ft: ex03 completed

--- SOLID/ex03/src/Demo03.java
+++ SOLID/ex03/src/Demo03.java
@@ -1,5 +1,10 @@
 public class Demo03 {
     public static void main(String[] args) {
-        System.out.println(new ShippingCostCalculator().cost(new Shipment("EXPRESS", 2.0)));
+        Shipment s1 = new Standard(10);
+        System.out.println("Standard shipment cost for 10kg: " + s1.getcost());
+        Shipment s2 = new Express(10);
+        System.out.println("Express shipment cost for 10kg: " + s2.getcost());
+        Shipment s3 = new Overnight(10);
+        System.out.println("Overnight shipment cost for 10kg: " + s3.getcost());
     }
 }
