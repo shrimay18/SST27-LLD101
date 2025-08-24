89da38bf81bfd73952f6f5a4dbc36404f63f80af
Shrimay <shrimaysomani18@gmail.com>
2025-08-25 02:00:33 +0530

    ft: ex09 completed

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,9 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        OrderRepository sqlRepo = new SqlOrderRepository();
+        OrderController controller = new OrderController(sqlRepo);
+
+        controller.create("ORD-1");
     }
 }
+
--- SOLID/ex09/src/OrderController.java
+++ SOLID/ex09/src/OrderController.java
@@ -1,6 +1,11 @@
 public class OrderController {
-    void create(String id){
-        SqlOrderRepository repo = new SqlOrderRepository(); // hard dependency
+    private final OrderRepository repo;
+
+    public OrderController(OrderRepository repo) {
+        this.repo = repo;
+    }
+
+    public void create(String id) {
         repo.save(id);
         System.out.println("Created order: " + id);
     }
