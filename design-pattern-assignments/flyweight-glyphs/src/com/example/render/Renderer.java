2b58032b4c3bff10f839ef94c82b110540fb111f
Shrimay <shrimaysomani18@gmail.com>
2025-09-07 19:21:28 +0530

    flyweight-glyphs assignment completed

--- design-pattern-assignments/flyweight-glyphs/src/com/example/render/Renderer.java
+++ design-pattern-assignments/flyweight-glyphs/src/com/example/render/Renderer.java
@@ -1,10 +1,15 @@
 package com.example.render;
 
+
 public class Renderer {
+
+    private final TextStyleFactory styleFactory = new TextStyleFactory();
+
     public int render(String text) {
         int cost = 0;
         for (char c : text.toCharArray()) {
-            Glyph g = new Glyph(c, "Inter", 14, (c % 7 == 0)); // TODO: refactor with TextStyle flyweights
+            TextStyle style = styleFactory.getTextStyle("Arial", 12, false);
+            Glyph g = new Glyph(c, style);
             cost += g.drawCost();
         }
         return cost;
